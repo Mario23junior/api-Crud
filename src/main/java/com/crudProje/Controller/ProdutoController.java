afea932ab5e016c7b24092e2cbd0eabaadc66783
Mario_junior23 <mariojunior3251@gmail.com>
2020-09-14 23:34:27 -0300

    listando produtos por id de identificacao

--- src/main/java/com/crudProje/Controller/ProdutoController.java
+++ src/main/java/com/crudProje/Controller/ProdutoController.java
@@ -1,15 +1,19 @@
 package com.crudProje.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudProje.Model.Produtos;
 import com.crudProje.Repository.ProdutoRepository;
-
+  
 @RequestMapping("/api")
 @RestController
 public class ProdutoController {
@@ -22,4 +26,25 @@ public class ProdutoController {
  	 public List<Produtos>list(){
 		   return produtoRepository.findAll();
 	 }
+	
+	// listando produto por indentificador id
+	@GetMapping(value = "/listId/{id}")
+	public ResponseEntity<Produtos> listId(@PathVariable(value = "id") long id){
+		 Optional<Produtos> produto = produtoRepository.findById(id);
+		  if(produto.isPresent()) {
+			  return new ResponseEntity<Produtos>(produto.get(), HttpStatus.OK);
+		  }else {
+			  return new ResponseEntity<Produtos>(HttpStatus.NOT_FOUND);
+		  }
+	}
 }
+
+
+
+
+
+
+
+
+
+
