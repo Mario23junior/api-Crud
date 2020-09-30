e54c6db80978509df4da7f6c2e57d071a32c1415
Mario_junior23 <mariojunior3251@gmail.com>
2020-09-30 04:03:02 -0300

    Configurando Spring Security

--- src/main/java/com/crudProje/Config/ConfigSecurity.java
+++ src/main/java/com/crudProje/Config/ConfigSecurity.java
@@ -1,13 +1,40 @@
 package com.crudProje.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-@EnableResourceServer
-@EnableAuthorizationServer
 public class ConfigSecurity extends WebSecurityConfigurerAdapter{
+	
+	@Override
+   public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	   
+		 auth. 
+		  inMemoryAuthentication()
+			  .withUser("nameuser")
+			  .password("123")
+			  .roles("USER");
+   }
+	
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+		
+	}
+	
+	@Override
+ 	protected void configure(HttpSecurity http) throws Exception {
+	 http.
+		csrf().disable()
+		.cors()
+	 .and()
+	      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	 
+	}
 
 }
