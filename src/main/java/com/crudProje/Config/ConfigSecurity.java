1548246b60ee58e91188aead4e845ef2e5710b80
Mario_junior23 <mariojunior3251@gmail.com>
2020-09-30 03:24:12 -0300

    A dicionando as dependêcias do Spring Security e testanto a configuração padrão

---
+++ src/main/java/com/crudProje/Config/ConfigSecurity.java
@@ -0,0 +1,13 @@
+package com.crudProje.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+@EnableAuthorizationServer
+public class ConfigSecurity extends WebSecurityConfigurerAdapter{
+
+}
