6ccd9c20bfd67350f6ff918c335f1ea61302f0f3
Mario_junior23 <mariojunior3251@gmail.com>
2020-09-30 04:38:41 -0300

    Configurando o Resource Server

--- src/main/java/com/crudProje/Config/ResourceServerConfig.java
+++ src/main/java/com/crudProje/Config/ResourceServerConfig.java
@@ -1,5 +1,21 @@
 package com.crudProje.Config;
 
-public class ResourceServerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+   @Override
+   public void configure(HttpSecurity http) throws Exception {
+	   http
+	   .authorizeRequests()
+	   .antMatchers("/usuario", "/api/save**").authenticated()
+	   
+	   .anyRequest().denyAll();
+	   ;
+   }
 }
