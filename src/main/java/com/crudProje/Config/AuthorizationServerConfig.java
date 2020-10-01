32adde3f28b20d7ece8fd7b847f94671db4606b6
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-01 01:32:10 -0300

    Configurando o OAuth para utilizar tokens JWT

--- src/main/java/com/crudProje/Config/AuthorizationServerConfig.java
+++ src/main/java/com/crudProje/Config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.crudProje.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,35 +10,47 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
     
+	@Value("${security.jwt.signing-key}")
+	private String signingKey;
+	
 	@Autowired
      private AuthenticationManager authenticationManager;
 	
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(AccessTokenConverter());
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter AccessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(signingKey);
+		return tokenConverter;
 	}
      
      @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
     	 endpoints
     	 .tokenStore(tokenStore())
+    	 .accessTokenConverter(AccessTokenConverter())
     	 .authenticationManager(authenticationManager);
      }
      
      @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
     	 clients
-    	 .inMemory()
-    	 .withClient("my-app")
-    	 .secret("@394")
-    	 .scopes("read", "write")
-    	 .authorizedGrantTypes("password")
-    	 .accessTokenValiditySeconds(1800);
+	    	 .inMemory()
+	    	 .withClient("my-app")
+	    	 .secret("@394")
+	    	 .scopes("read", "write")
+	    	 .authorizedGrantTypes("password")
+	    	 .accessTokenValiditySeconds(1800);
      }
 }
